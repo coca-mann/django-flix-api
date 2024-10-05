d78755e5878ce698a01245e4ccb0da00ef37b962
Coquinhaaa <juliano.ostroski@gmail.com>
2024-10-04 23:51:23 -0400

    Criar nova rota para método GET em genres/id; Criar nova view para tratar requisição GET com id do objeto ou retornar 404

--- apps/genres/views.py
+++ apps/genres/views.py
@@ -1,13 +1,16 @@
 import json
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+from django.shortcuts import get_object_or_404
 from apps.genres.models import Genre
 
 @csrf_exempt
-def genre_view(request):
+def genre_create_list_view(request):
     if request.method == 'GET':
         genres = Genre.objects.all()
-        data = [{'id': genre.id, 'name': genre.name} for genre in genres]
+        data = [
+            {'id': genre.id, 'name': genre.name} for genre in genres
+        ]
         return JsonResponse(data, safe=False)
     elif request.method == 'POST':
         data = json.loads(request.body.decode('utf8'))
@@ -17,3 +20,9 @@ def genre_view(request):
             {'id': new_genre.id, 'name': new_genre.name},
             status=201,
         )
+
+@csrf_exempt
+def genre_detail_view(request, pk):
+    genre = get_object_or_404(Genre, pk=pk)
+    data = {'id': genre.id, 'name': genre.name}
+    return JsonResponse(data)

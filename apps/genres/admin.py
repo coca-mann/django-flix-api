dca11226143dd949a61e8e1c47e05daf280cab0a
Coquinhaaa <juliano.ostroski@gmail.com>
2024-10-04 22:40:46 -0400

    Listar model Genre no admin do Django

--- apps/genres/admin.py
+++ apps/genres/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from apps.genres.models import Genre
 
-# Register your models here.
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')

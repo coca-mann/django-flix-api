a56dd75ad75c60b6293eb8b80c95fcfa6c3f2cf4
Coquinhaaa <juliano.ostroski@gmail.com>
2024-10-05 18:06:26 -0400

    Criar tratamento para método DELETE na view 'genre_detail_view'

--- apps/genres/views.py
+++ apps/genres/views.py
@@ -33,5 +33,11 @@ def genre_detail_view(request, pk):
         genre.save()
         return JsonResponse(
             {'id': genre.id, 'name': genre.name},
-            
+            status=200,
+        )
+    elif request.method == 'DELETE':
+        genre.delete()
+        return JsonResponse(
+            {'message': 'Gênero excluído com sucesso!'},
+            status=204,
         )

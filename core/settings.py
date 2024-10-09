3f40c99b70edd99be12cf475d59c6151586b0a10
Coquinhaaa <juliano.ostroski@gmail.com>
2024-10-08 21:48:49 -0400

    Adicionar app actors aos aplicativos instalados

--- core/settings.py
+++ core/settings.py
@@ -40,8 +40,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'apps.genres',
     'rest_framework',
+    'apps.genres',
+    'apps.actors',
 ]
 
 MIDDLEWARE = [

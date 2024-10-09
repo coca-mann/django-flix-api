03c3d6e34e32400d9c7e271404c40830ba0157dc
Coquinhaaa <juliano.ostroski@gmail.com>
2024-10-09 00:07:34 -0400

    App Reviews > Criar serializer; Criar views com CRUD completo; Criar rotas de URL para app

--- core/urls.py
+++ core/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from apps.genres.views import GenreCreateListView, GenreRetrieveUpdateDestroyView
 from apps.actors.views import ActorCreatListView, ActorRetrieveUpdateDestroyView
 from apps.movies.views import MovieCreateListView, MovieRetrieveUpdateDestroyView
+from apps.reviews.views import ReviewCreateListView, ReviewRetrieveUpdateDestroyView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -11,5 +12,7 @@ urlpatterns = [
     path('actors/', ActorCreatListView.as_view(), name='actor-create-list'),
     path('actors/<int:pk>', ActorRetrieveUpdateDestroyView.as_view(), name='actor-detail-view'),
     path('movies/', MovieCreateListView.as_view(), name='movies-create-list'),
-    path('movies/<int:pk>', MovieRetrieveUpdateDestroyView.as_view(), name='movie-detail-view')
+    path('movies/<int:pk>', MovieRetrieveUpdateDestroyView.as_view(), name='movie-detail-view'),
+    path('reviews/', ReviewCreateListView.as_view(), name='review-create-list'),
+    path('reviews/<int:pk>', ReviewRetrieveUpdateDestroyView.as_view(), name='review-detail-view'),
 ]

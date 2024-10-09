ef7750c8cde41c9d0a9a410842b4cbe25b3f85fb
Coquinhaaa <juliano.ostroski@gmail.com>
2024-10-08 22:17:16 -0400

    Criar rotas para app Actors

--- core/urls.py
+++ core/urls.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
 from django.urls import path
 from apps.genres.views import GenreCreateListView, GenreRetrieveUpdateDestroyView
+from apps.actors.views import ActorCreatListView, ActorRetrieveUpdateDestroyView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('genres/', GenreCreateListView.as_view(), name='genre-create-list'),
-    path('genres/<int:pk>', GenreRetrieveUpdateDestroyView.as_view(), name='genre-detail-view')
+    path('genres/<int:pk>', GenreRetrieveUpdateDestroyView.as_view(), name='genre-detail-view'),
+    path('actors/', ActorCreatListView.as_view(), name='actor-create-list'),
+    path('actors/<int:pk>', ActorRetrieveUpdateDestroyView.as_view(), name='actor-detail-view'),
 ]
-
-
-

b92a2a5f549112cb58854683ac26d12041c3a42a
Coquinhaaa <juliano.ostroski@gmail.com>
2024-10-08 23:26:17 -0400

    Criar rota de URL para views do app Movies

--- core/urls.py
+++ core/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path
 from apps.genres.views import GenreCreateListView, GenreRetrieveUpdateDestroyView
 from apps.actors.views import ActorCreatListView, ActorRetrieveUpdateDestroyView
+from apps.movies.views import MovieCreateListView, MovieRetrieveUpdateDestroyView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -9,4 +10,6 @@ urlpatterns = [
     path('genres/<int:pk>', GenreRetrieveUpdateDestroyView.as_view(), name='genre-detail-view'),
     path('actors/', ActorCreatListView.as_view(), name='actor-create-list'),
     path('actors/<int:pk>', ActorRetrieveUpdateDestroyView.as_view(), name='actor-detail-view'),
+    path('movies/', MovieCreateListView.as_view(), name='movies-create-list'),
+    path('movies/<int:pk>', MovieRetrieveUpdateDestroyView.as_view(), name='movie-detail-view')
 ]

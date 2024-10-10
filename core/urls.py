69d7aecc03570a464cbb04eb4a44090b2d59c4f7
Coquinhaaa <juliano.ostroski@gmail.com>
2024-10-09 22:46:38 -0400

    Criar em todas as apps o arquivo 'urls.py'; Definir nova rota api/v1 com função include para incluir urls de dentro dos apps

--- core/urls.py
+++ core/urls.py
@@ -1,18 +1,11 @@
 from django.contrib import admin
-from django.urls import path
-from apps.genres.views import GenreCreateListView, GenreRetrieveUpdateDestroyView
-from apps.actors.views import ActorCreatListView, ActorRetrieveUpdateDestroyView
-from apps.movies.views import MovieCreateListView, MovieRetrieveUpdateDestroyView
-from apps.reviews.views import ReviewCreateListView, ReviewRetrieveUpdateDestroyView
+from django.urls import path, include
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('genres/', GenreCreateListView.as_view(), name='genre-create-list'),
-    path('genres/<int:pk>', GenreRetrieveUpdateDestroyView.as_view(), name='genre-detail-view'),
-    path('actors/', ActorCreatListView.as_view(), name='actor-create-list'),
-    path('actors/<int:pk>', ActorRetrieveUpdateDestroyView.as_view(), name='actor-detail-view'),
-    path('movies/', MovieCreateListView.as_view(), name='movies-create-list'),
-    path('movies/<int:pk>', MovieRetrieveUpdateDestroyView.as_view(), name='movie-detail-view'),
-    path('reviews/', ReviewCreateListView.as_view(), name='review-create-list'),
-    path('reviews/<int:pk>', ReviewRetrieveUpdateDestroyView.as_view(), name='review-detail-view'),
+    path('api/v1/', include('apps.genres.urls')),
+    path('api/v1/', include('apps.actors.urls')),
+    path('api/v1/', include('apps.movies.urls')),
+    path('api/v1/', include('apps.reviews.urls')),
 ]

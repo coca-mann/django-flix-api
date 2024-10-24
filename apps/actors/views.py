010795fe6cb194195c480b2e60b3d76f9bd671e0
Coquinhaaa <juliano.ostroski@gmail.com>
2024-10-23 21:34:51 -0400

    Adicionar parametro permission_classes para todas as views dos apps

--- apps/actors/views.py
+++ apps/actors/views.py
@@ -1,13 +1,16 @@
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 from apps.actors.models import Actor
 from apps.actors.serializers import ActorSerializer
 
 
 class ActorCreatListView(generics.ListCreateAPIView):
+    permission_classes = (IsAuthenticated,)
     queryset = Actor.objects.all()
     serializer_class = ActorSerializer
 
 
 class ActorRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthenticated,)
     queryset = Actor.objects.all()
     serializer_class = ActorSerializer
--- apps/movies/views.py
+++ apps/movies/views.py
@@ -1,13 +1,16 @@
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 from apps.movies.models import Movie
 from apps.movies.serializers import MovieSerializer
 
 
 class MovieCreateListView(generics.ListCreateAPIView):
+    permission_classes = (IsAuthenticated,)
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
 
 
 class MovieRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthenticated,)
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer

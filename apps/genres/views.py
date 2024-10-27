c6dc6bcd6cf9f5b15b079a224c9b80a2819c0a16
Coquinhaaa <juliano.ostroski@gmail.com>
2024-10-27 19:04:50 -0400

    Criar nova classe de permissão e adicionar a uma view na app Genre

--- apps/genres/views.py
+++ apps/genres/views.py
@@ -2,10 +2,11 @@ from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
 from apps.genres.models import Genre
 from apps.genres.serializers import GenreSerializer
+from apps.genres.permissions import GenrePermissionClass
 
 
 class GenreCreateListView(generics.ListCreateAPIView):
-    permission_classes = (IsAuthenticated,)
+    permission_classes = (IsAuthenticated, GenrePermissionClass,)
     queryset = Genre.objects.all()
     serializer_class = GenreSerializer
 
